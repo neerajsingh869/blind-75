66e97dd6cd39730523adb3637efe7b5851778fbd
Neeraj Singh <869neerajsingh@gmail.com>
2025-01-01 23:49:57 +0530

    feat: Concise approach to solve same tree check problem

--- trees/SameTree.js
+++ trees/SameTree.js
@@ -14,61 +14,66 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
 /**
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {boolean}
  */
 // DFS (Time complexity -> O(min(n, m)), Space complexity -> O(min(hn, hm)))
-var isSameTree1 = function(p, q) {
+var isSameTree1 = function (p, q) {
   if (!p && !q) return true;
   if (!p && q) return false;
   if (!q && p) return false;
-  
+
   if (p.val != q.val) return false;
 
-  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  return isSameTree1(p.left, q.left) && isSameTree1(p.right, q.right);
 };
 
 // Mimic recursion stack (Time complexity -> O(min(n, m)), Space complexity -> O(min(n, m)))
-var isSameTree2 = function(p, q) {
+var isSameTree2 = function (p, q) {
   if (!p && !q) return true;
   if (!p && q) return false;
   if (!q && p) return false;
-  
+
   let stack1 = [];
   let stack2 = [];
-  
+
   stack1.push(p);
   stack2.push(q);
 
   while (stack1.length && stack2.length) {
-      let currNodeP = stack1.pop();
-      let currNodeQ = stack2.pop();
+    let currNodeP = stack1.pop();
+    let currNodeQ = stack2.pop();
 
-      if (currNodeP.val != currNodeQ.val) return false;
+    if (currNodeP.val != currNodeQ.val) return false;
 
-      if (currNodeP.left && currNodeQ.left && 
-          (currNodeP.left.val === currNodeQ.left.val)) 
-      {
-          stack1.push(currNodeP.left);
-          stack2.push(currNodeQ.left);
-      } else {
-          if (!(!currNodeP.left && !currNodeQ.left)) {
-              return false;
-          }
+    if (
+      currNodeP.left &&
+      currNodeQ.left &&
+      currNodeP.left.val === currNodeQ.left.val
+    ) {
+      stack1.push(currNodeP.left);
+      stack2.push(currNodeQ.left);
+    } else {
+      if (!(!currNodeP.left && !currNodeQ.left)) {
+        return false;
       }
+    }
 
-      if (currNodeP.right && currNodeQ.right && 
-          (currNodeP.right.val === currNodeQ.right.val)) 
-      {
-          stack1.push(currNodeP.right);
-          stack2.push(currNodeQ.right);
-      } else {
-          if (!(!currNodeP.right && !currNodeQ.right)) {
-              return false;
-          }
+    if (
+      currNodeP.right &&
+      currNodeQ.right &&
+      currNodeP.right.val === currNodeQ.right.val
+    ) {
+      stack1.push(currNodeP.right);
+      stack2.push(currNodeQ.right);
+    } else {
+      if (!(!currNodeP.right && !currNodeQ.right)) {
+        return false;
       }
+    }
   }
 
   if (stack1.length || stack2.length) return false;
@@ -76,8 +81,8 @@ var isSameTree2 = function(p, q) {
   return true;
 };
 
-// BFS (Time complexity -> O(min(n, m)), Space complexity -> O(min(n, m)))
-var isSameTree3 = function(p, q) {
+// BFS (Time complexity -> O(min(n, m) * min(n, m)), Space complexity -> O(min(n, m)))
+var isSameTree3 = function (p, q) {
   if (!p && !q) return true;
   if (!p && q) return false;
   if (p && !q) return false;
@@ -89,38 +94,86 @@ var isSameTree3 = function(p, q) {
   queue2.push(q);
 
   while (queue1.length && queue2.length) {
-      let currNodeP = queue1[0];
-      let currNodeQ = queue2[0];
+    let currNodeP = queue1[0];
+    let currNodeQ = queue2[0];
 
-      queue1.shift();
-      queue2.shift();
+    queue1.shift();
+    queue2.shift();
 
-      if (currNodeP.val != currNodeQ.val) return false;
+    if (currNodeP.val != currNodeQ.val) return false;
 
-      if (currNodeP.left && currNodeQ.left && 
-        (currNodeP.left.val === currNodeQ.left.val)) 
-      {
-          queue1.push(currNodeP.left);
-          queue2.push(currNodeQ.left);
-      } else {
-          if (!(!currNodeP.left && !currNodeQ.left)) {
-              return false;
-          }
+    if (
+      currNodeP.left &&
+      currNodeQ.left &&
+      currNodeP.left.val === currNodeQ.left.val
+    ) {
+      queue1.push(currNodeP.left);
+      queue2.push(currNodeQ.left);
+    } else {
+      if (!(!currNodeP.left && !currNodeQ.left)) {
+        z``;
+        return false;
       }
+    }
 
-      if (currNodeP.right && currNodeQ.right && 
-          (currNodeP.right.val === currNodeQ.right.val)) 
-      {
-          queue1.push(currNodeP.right);
-          queue2.push(currNodeQ.right);
-      } else {
-          if (!(!currNodeP.right && !currNodeQ.right)) {
-              return false;
-          }
+    if (
+      currNodeP.right &&
+      currNodeQ.right &&
+      currNodeP.right.val === currNodeQ.right.val
+    ) {
+      queue1.push(currNodeP.right);
+      queue2.push(currNodeQ.right);
+    } else {
+      if (!(!currNodeP.right && !currNodeQ.right)) {
+        return false;
       }
+    }
   }
 
   if (queue1.length || queue2.length) return false;
 
   return true;
 };
+
+/**
+ * 
+ * @param {TreeNode} a 
+ * @param {TreeNode} b 
+ * @returns {boolean}
+ */
+var isSameTree4 = function (a, b) {
+  let queue = [];
+  queue.push([a, b]);
+
+  while (queue.length) {
+    const [a, b] = queue.shift();
+
+    if (!check(a, b)) {
+      return false;
+    }
+
+    if (a && b) {
+      queue.push([a.left, b.left]);
+      queue.push([a.right, b.right]);
+    }
+  }
+
+  return true;
+};
+
+// Utility method to check if node a and b are equal
+function check(a, b) {
+  if (a === null && b === null) {
+    return true;
+  }
+
+  if (a === null || b === null) {
+    return false;
+  }
+
+  if (a.val !== b.val) {
+    return false;
+  }
+
+  return true;
+}
