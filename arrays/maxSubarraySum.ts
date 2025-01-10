f12f7167c2ae2ee780da79c6afe853c056eac114
Neeraj Singh <869neerajsingh@gmail.com>
2025-01-10 20:54:52 +0530

    feat: Solve max subarray sum problem & Update the code from JS to TS

--- arrays/maxSubarraySum.ts
+++ arrays/maxSubarraySum.ts
@@ -1,37 +1,42 @@
 /**
-  Given an integer array nums, find the 
-  subarray
-  with the largest sum, and return its sum.
+ * PROBLEM LINK: https://www.greatfrontend.com/interviews/study/blind75/questions/algo/array-maximum-sum-contiguous
+ * 
+ * Given an array of integers numbers, determine the subarray 
+ * that has the highest sum and return that sum.
 
-  Example 1:
-  Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-  Output: 6
-  Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+  A subarray is a contiguous segment of an array where all elements 
+  are taken from consecutive indices, preserving their order, 
+  such as [2, 3] in [1, 2, 3, 4], while non-contiguous selections 
+  like [1, 3] are not valid subarray.
 
-  Example 2:
-  Input: nums = [1]
-  Output: 1
-  Explanation: The subarray [1] has the largest sum 1.
+  Input
+  numbers: number[]: An array of integers
 
-  Example 3:
-  Input: nums = [5,4,-1,7,8]
-  Output: 23
-  Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+  Examples
+  Input: numbers = [-1,5,-3,9,-11]
+  Output: 11
+  Explanation: The subarray [5, -3, 9] has the largest sum i.e 11.
 
-  Constraints:
-  1 <= nums.length <= 105
-  -104 <= nums[i] <= 104
+  Input: numbers = [9]
+  Output: 9
+  Explanation: The single-element subarray [9] has the largest sum i.e 9.
 
-  Follow up: If you have figured out the O(n) solution, try coding 
-  another solution using the divide and conquer approach, which is more subtle.
+  Input: numbers = [1,2,3,4]
+  Output: 10
+  Explanation: The subarray [1,2,3,4] has the largest sum i.e 10.
+  
+  Constraints
+  1 <= numbers.length <= 10,000
+  -10,000 <= numbers[i] <= 10,000
  */
+
 /**
- * @param {number[]} nums
+ * @param nums 
  * Time complexity => O(n * n * n)
  * Space complexity => O(1)
- * @return {number}
+ * @returns 
  */
-var maxSubArray1 = function (nums) {
+var maxSubArray1 = function (nums: number[]): number {
   let n = nums.length;
   let maxSubArraySum = -Infinity;
 
@@ -51,12 +56,12 @@ var maxSubArray1 = function (nums) {
 };
 
 /**
- * @param {number[]} nums
+ * @param nums 
  * Time complexity => O(n * n)
  * Space complexity => O(1)
- * @return {number}
+ * @returns 
  */
-var maxSubArray2 = function (nums) {
+var maxSubArray2 = function (nums: number[]): number {
   let n = nums.length;
   let maxSubArraySum = -Infinity;
 
@@ -73,12 +78,12 @@ var maxSubArray2 = function (nums) {
 };
 
 /**
- * @param {number[]} nums
+ * @param nums 
  * Time complexity => O(n)
  * Space complexity => O(1)
- * @return {number}
+ * @returns 
  */
-var maxSubArray3 = function (nums) {
+var maxSubArray3 = function (nums: number[]): number {
   let n = nums.length;
   let maxSubArraySum = -Infinity;
   let tempSum = 0;
@@ -99,13 +104,12 @@ var maxSubArray3 = function (nums) {
 };
 
 /**
- * @param {number[]} nums
- * Kadane's algorithm
+ * @param nums 
  * Time complexity => O(n)
  * Space complexity => O(1)
- * @return {number}
+ * @returns 
  */
-var maxSubArray4 = function (nums) {
+var maxSubArray4 = function (nums: number[]): number {
   if (nums.length === 0) return 0;
 
   let maxSum = nums[0]; // Keeps track of global maxima
@@ -123,13 +127,12 @@ var maxSubArray4 = function (nums) {
 };
 
 /**
- * @param {number[]} nums
- * Kadane's algorithm
- * Time complexity => O(n log n)
- * Space complexity => O(log n)
- * @return {number}
+ * @param nums 
+ * Time complexity => O(n * log(n))
+ * Space complexity => O(log(n))
+ * @returns 
  */
-var maxSubArray5 = function (nums) {
+var maxSubArray5 = function (nums: number[]): number {
   return divideAndConquer(nums, 0, nums.length - 1);
 };
 
@@ -167,14 +170,14 @@ function divideAndConquer(nums, left, right) {
 }
 
 /**
- * @param {number[]} nums
+ * @param nums 
  * Problem Extension: Return subarray and not the sum
  * Kadane's algorithm
  * Time complexity => O(n)
  * Space complexity => O(1)
- * @return {number}
+ * @returns 
  */
-function maxArrayWithIndices(nums) {
+function maxArrayWithIndices(nums: number[]): number[] {
   if (nums.length === 0) return [];
 
   // Initialization
