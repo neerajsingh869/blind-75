fcb7a5776ff50a670f3932aeebf4ad06eb251f9f
Neeraj Singh <869neerajsingh@gmail.com>
2025-01-29 16:32:08 +0530

    feat: Solve build binary tree using preorder and inorder problem

---
+++ trees/buildPreorder&Inorder.ts
@@ -0,0 +1,102 @@
+/**
+ * PROBLEM LINK: https://www.greatfrontend.com/interviews/study/blind75/questions/algo/binary-tree-rebuilding-from-traversals
+ * 
+ * Given two arrays of integers, preorder and inorder, 
+ * where preorder represents the values of a preorder 
+ * traversal of a binary tree, and inorder represents 
+ * the values of an inorder traversal of the same tree, 
+ * construct and return the binary tree from these traversals.
+
+  Input
+  preorder: number[]: An array of positive integers
+  inorder: number[]: An array of positive integers
+
+  Examples
+  Input: preorder = [3,1,2,6,5,9], inorder = [1,2,3,5,6,9]
+  Output: [3,1,6,null,2,5,9]
+  Explanation: The root is 3, with left subtree [1, 2] and right subtree [6, 5, 9].
+
+  Input: preorder = [1,2,3], inorder = [3,2,1]
+  Output: [1,2,null,3]
+  Explanation: The root is 1, with left subtree [2, 3] and no right subtree.
+  
+  Input: preorder = [7], inorder = [7]
+  Output: [7]
+  Explanation: The tree consists of a single node with the 
+  value 7, which is both the root and the only node.
+
+  Constraints
+  1 <= Number of nodes <= 1000
+  1 <= TreeNode.val <= 1,000,000
+  preorder and inorder contain unique values
+ */
+
+interface TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+}
+
+/**
+ * @param preorder
+ * @param inorder
+ * TC = O(n * n)
+ * SC = O(h)
+ * @returns
+ */
+function binaryTreeRebuildingFromTraversals1(
+  preorder: number[],
+  inorder: number[]
+): TreeNode | null {
+  const n = preorder.length;
+  const m = inorder.length;
+
+  if (n != m) return null;
+
+  return buildBinaryTree(preorder, 0, n - 1, inorder, 0, m - 1);
+}
+
+function buildBinaryTree(
+  preorder: number[],
+  preStart: number,
+  preEnd: number,
+  inorder: number[],
+  inStart: number,
+  inEnd: number
+): TreeNode | null {
+  if (preStart > preEnd || inStart > inEnd) {
+    return null;
+  }
+
+  const rootValue = preorder[preStart];
+
+  let rootInorderIndex = inStart;
+  for (let i = inStart; i <= inEnd; i++) {
+    if (inorder[i] === rootValue) {
+      rootInorderIndex = i;
+      break;
+    }
+  }
+
+  const root: TreeNode = { val: rootValue, left: null, right: null };
+
+  root.left = buildBinaryTree(
+    preorder,
+    preStart + 1,
+    preStart + rootInorderIndex - inStart,
+    inorder,
+    inStart,
+    rootInorderIndex - 1
+  );
+
+  root.right = buildBinaryTree(
+    preorder,
+    preStart + rootInorderIndex - inStart + 1,
+    preEnd,
+    inorder,
+    rootInorderIndex + 1,
+    inEnd
+  );
+
+  return root;
+}
